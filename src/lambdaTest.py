34dbb18dbe0b9f7b872272d47a4ff615d55e84eb
Brendan Davis <brendanmd@gmail.com>
2017-11-13 14:53:11 -0800

    Tested those lambdas

---
+++ src/lambdaTest.py
@@ -0,0 +1,42 @@
+import random
+
+dinnerOptions = [
+    "Chicken",
+    "Beef",
+    "Pork",
+    "Fish",
+    "Vegetarian"
+]
+
+sides = [
+    "Mashed Potatoes"
+    "Macaroni and Cheese"
+    "Asparagus"
+]
+
+def lambda_handler(event, context):
+    print("Damnit alexa don't be a bitch")
+    if event["request"]["intent"]["name"] == "DinnerBotIntent": 
+        dinner = random.choice(dinnerOptions)
+        response = {
+            'version': '1.0',
+            'response': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': dinner,
+                }
+            }
+        }
+    else:
+        app = random.choice(sides)
+        response = {
+            'version': '1.0',
+            'response': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': app,
+                }
+            }
+        }
+        
+    return response
