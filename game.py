18000e2d231da881e5e116f38ab3c39c7abe304a
Allan Liebold <allan.liebold@gmail.com>
2017-11-13 13:53:04 -0800

    adds function for info, lets player quit to menu and out of the game, creates end scene argument for Scene class so the game knows you've reached a game over state.

--- game.py
+++ game.py
@@ -1,17 +1,18 @@
 """Text adventure tutorial."""
 import random
 import time
+import sys
 
 
 class Scene(object):
     """Scene class object."""
 
-    def __init__(self, scene_id, body):
+    def __init__(self, scene_id, body, end_scene=False):
         """Initialization of scene object."""
         self.scene_id = scene_id
         self.body = body
         self.choices = []
-
+        self.end_scene = end_scene
 
 class Story(object):
     """Story class object."""
@@ -21,9 +22,9 @@ class Story(object):
         self.title = 'Title'
         self.scenes = {}
 
-    def add_scene(self, scene_id, body):
+    def add_scene(self, scene_id, body, end_scene=False):
         """Create a scene and add it to scenes list."""
-        new_scene = Scene(scene_id, body)
+        new_scene = Scene(scene_id, body, end_scene)
         if scene_id in self.scenes:
             raise ValueError('Duplicate scene.')
         self.scenes[scene_id] = new_scene
@@ -54,48 +55,79 @@ def display_intro(story):
 
 def main_prompt(story):
     """Ask for user input."""
-    print('Choose an option.')
-    user_response = input('1 to play. Q to quit. \n')
+    print('\n Main menu \n ---------- \n Choose an option.')
+    user_response = input('1 to play. Q to quit. Type info or help for game instructions. \n')
     if user_response == '1':
         print('Starting game.')
         choose_path(story, story.scenes[1])
-    elif user_response.lower() == 'q':
-        print('Selected quit.')
+    elif user_response.lower() in ('q', 'quit', 'exit'):
+        print('Are you sure you want to quit?')
+        quit = input('Y or N: ')
+        if quit.lower() in ('y', 'yes'):
+            sys.exit()
+    elif user_response.lower() in ('i', 'info', 'information', 'help', 'h', 'instructions'):
+        info()
     else:
         print('Invalid response. \n')
-    main_prompt()
+    main_prompt(story)
 
 
 def choose_path(story, scene):
     """User prompt for game."""
     print(scene.body)
+    if scene.end_scene:
+        time.sleep(1)
+        print("Thanks for playing!")
+        time.sleep(3)
+        main_prompt(story)
     path = ""
     while path != "1" and path != "2":
-        path = input("Choose 1 or 2. Q to quit. \n")
+        path = input("Make a choice: ")
         if path == "1":
             choose_path(story, story.scenes[scene.choices[0].scene_id])
         elif path == "2":
-            choose_path(story, story.scenes[scene.choices[1].scene_id])
-        elif path.lower() == 'q':
-            print('Selected quit. \n')
+            try:
+                choose_path(story, story.scenes[scene.choices[1].scene_id])
+            except:
+                print('Nope.')
+        elif path.lower() in ("info", "information", "help", "h", "instructions"):
+            info()
+            print(scene.body)
+        elif path.lower() in ('q', 'quit', 'exit'):
+            print('Are you sure you want to quit to the menu?\n')
+            quit = input('Y or N: ')
+            if quit.lower() in ('y', 'yes'):
+                main_prompt(story)
+            else:
+                print(scene.body)
         else:
             print('Invalid response. \n')
 
 
 def info():
     """User asks for help or information."""
-    pass
+    print("\n Game instructions \n")
+    print("-----------------------")
+    print("This is where game instructions go.")
+    print("Alexa will read a description of the current scene.")
+    print("Then there will be a prompt asking the player to choose from set options.")
+    print("The next scene will be called based on the player's response.")
+    print("At any point the player may quit back to the main menu. \n")
 
 
 if __name__ == '__main__':
     test_story = Story('Test Story')
-    test_story.add_scene(1, "This is the first scene. 1) Go right or 2) Go left.")
-    test_story.add_scene(2, "You went right. 1) Go back or 2) Go left.")
-    test_story.add_scene(3, "You went left. 1) Go back or 2) Go right.")
+    test_story.add_scene(1, "This is the first scene. 1) Go left or 2) Go right.")
+    test_story.add_scene(2, "You went left. There is a ladder leading up into sunlight. 1) Go back or 2) Go up.")
+    test_story.add_scene(3, "You went right. There are stairs leading down into darkness. 1) Go back or 2) Go down.")
+    test_story.add_scene(4, "You climb out. This is the end. Congratulations! \n", True)
+    test_story.add_scene(5, "You go down the stairs and get lost forever. The end. \n", True)
     test_story.add_path(1, 2)
     test_story.add_path(1, 3)
     test_story.add_path(2, 1)
-    test_story.add_path(2, 3)
+    test_story.add_path(2, 4)
     test_story.add_path(3, 1)
-    test_story.add_path(3, 2)
+    test_story.add_path(3, 5)
+    test_story.add_path(4, 4)
+    test_story.add_path(5, 5)
     display_intro(test_story)
