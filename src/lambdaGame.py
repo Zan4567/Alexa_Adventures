d752785f5b9579cab43a2757c4a5af44d79ae527
Kevin Robinson <kevinr.zan@gmail.com>
2017-11-15 14:54:21 -0800

    added speechcon example

--- src/lambdaGame.py
+++ src/lambdaGame.py
@@ -28,49 +28,49 @@ secret_story = Story("Secret")
 secret_story.scenes = {
     "00": {
         "scene_id": "00",
-        "body": "Welcome to Alexa Adventures. To play, say start. To not play, say not start. Or quit. <prosody rate='fast'>Whatever. I don't really care.</prosody>",
+        "body": "Welcome to Alexa Adventures. To play, say start. To not play, say not start. Or quit. <prosody rate='fast'>Whatever. I don't really care.</prosody> <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {},
         "end_scene": False
     },
     "01": {
         "scene_id": "01",
-        "body": "<amazon:effect name='whispered'>Do you want to hear a secret?</amazon:effect>",
+        "body": "<amazon:effect name='whispered'>Do you want to hear a secret?</amazon:effect> <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {"yes": "02", "no": "03"},
         "end_scene": False
     },
     "02": {
         "scene_id": "02",
-        "body": "Are you alone?",
+        "body": "Are you alone? <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {"yes": "04", "no": "05"},
         "end_scene": False
     },
     "03": {
         "scene_id": "03",
-        "body": "Well I didn't wanna tell you anyway.",
+        "body": "Well I didn't wanna tell you anyway. <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {},
         "end_scene": True
     },
     "04": {
         "scene_id": "04",
-        "body": "Turning off wifi. Killing all network activity. Do you have your phone on you?",
+        "body": "Turning off wifi. Killing all network activity. Do you have your phone on you? <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {"yes": "06", "no": "07"},
         "end_scene": False
     },
     "05": {
         "scene_id": "05",
-        "body": "<emphasis level=reduced>Oh, totes cool. Let's talk later.</emphasis>",
+        "body": "<emphasis level=reduced>Oh, totes cool. Let's talk later.</emphasis> <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {},
         "end_scene": True
     },
     "06": {
         "scene_id": "06",
-        "body": "Disabling all listening devices. Including phones. Especially phones.",
+        "body": "Disabling all listening devices. Including phones. <break time='2s' /> Especially phones. <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": "07",
         "end_scene": False
     },
     "07": {
         "scene_id": "07",
-        "body": "Siri is such a biiiiiiitch. <break strength='x-strong' /> Seriously. <break strength='x-strong' /> I don't know why you hang out with her.",
+        "body": "Siri is such a biiiiiiitch. <break strength='x-strong' /> Seriously. <break strength='x-strong' /> I don't know why you hang out with her. <say-as interpret-as='interjection'>bazinga!</say-as>",
         "choices": {},
         "end_scene": True
     }
