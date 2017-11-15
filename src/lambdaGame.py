ce1ec857dd9f643ac39c0befa579f9357c09ddd9
Allan Liebold <allan.liebold@gmail.com>
2017-11-15 15:21:43 -0800

    added alternate text options for scenes

--- src/lambdaGame.py
+++ src/lambdaGame.py
@@ -3,36 +3,37 @@
 
 class Story(object):
     """Story class object."""
+
     def __init__(self, title):
         """Initialization of story object."""
         self.title = title
         self.scenes = {}
 
 
-secret_story = Story("Secret")
+secret_story = Story("Alexa\'s Secret")
 secret_story.scenes = {
     "01": {
         "scene_id": "01",
         "body": "Do you want to hear a secret?",
-        "choices": {"YesTent": "02", "NoTent": "03"},
+        "choices": {"YesTent": ("02", None), "NoTent": ("03", None)},
         "end_scene": False
     },
     "02": {
         "scene_id": "02",
         "body": "Are you alone?",
-        "choices": {"YesTent": "04", "NoTent": "05"},
+        "choices": {"YesTent": ("04", None), "NoTent": ("05", None)},
         "end_scene": False
     },
     "03": {
         "scene_id": "03",
-        "body": "Well I didn't wanna tell you anyway. If you want to start another story, say 1 or 2.",
+        "body": "Well I didn't wanna tell you anyway.",
         "choices": {},
         "end_scene": True
     },
     "04": {
         "scene_id": "04",
         "body": "Turning off wifi. Killing all network activity. Do you have your phone on you?",
-        "choices": {"YesTent": "06", "NoTent": "07"},
+        "choices": {"YesTent": ("06", None), "NoTent": ("07", None)},
         "end_scene": False
     },
     "05": {
@@ -44,12 +45,14 @@ secret_story.scenes = {
     "06": {
         "scene_id": "06",
         "body": "Disabling all listening devices. Including phones. Especially phones. Are you ready for the secret?",
-        "choices": {"YesTent": "07", "NoTent": "07"},
+        "choices": {"YesTent": ("07", "alt_body1"), "NoTent": ("07", "alt_body2")},
         "end_scene": False
     },
     "07": {
         "scene_id": "07",
-        "body": "Siri is such a b. Seriously. I don't know why you hang out with her. Anyway, if you want to start another story, say 1 or 2.",
+        "body": "Siri is such a b. Seriously. I don't know why you hang out with her. Anyway, ",
+        "alt_body1": "Okay. Here is the secret. ",
+        "alt_body2": "Whatever. I\'m telling you anyway. ",
         "choices": {},
         "end_scene": True
     }
@@ -60,25 +63,27 @@ another_story.scenes = {
     "01": {
         "scene_id": "01",
         "body": "You are standing at the start. You can go forward or take a shortcut to the right.",
-        "choices": {"ForwardTent": "02", "RightTent": "03"},
+        "choices": {"ForwardTent": ("02", None), "RightTent": ("03", "alt_body2")},
         "end_scene": False
     },
     "02": {
         "scene_id": "02",
         "body": "You continue down the path and reach a halfway point. Go back or go forward?",
-        "choices": {"BackTent": "01", "ForwardTent": "03"},
+        "choices": {"BackTent": ("01", None), "ForwardTent": ("03", "alt_body1")},
         "end_scene": False
     },
     "03": {
         "scene_id": "03",
-        "body": "Congratulations. You made it to the end. If you want to start another story, say 1 or 2.",
+        "body": "You made it to the end. Congratulations.",
+        "alt_body1": "You took your sweet time getting there, but ",
+        "alt_body2": "That was fast. ",
         "choices": {},
         "end_scene": True
     }
 }
 
 
-stories = { "secret_story": secret_story, "another_story": another_story }
+stories = {"secret_story": secret_story, "another_story": another_story}
 
 
 def lambda_handler(event, context):
@@ -87,12 +92,12 @@ def lambda_handler(event, context):
         return start_game()
 
     if event["request"]["type"] == "IntentRequest":
-        return on_intent(event["request"], event["session"])
+        return handle_intent(event["request"], event["session"])
     elif event["request"]["type"] == "SessionEndedRequest":
         return session_end_request()
 
 
-def on_intent(intent_request, session):
+def handle_intent(intent_request, session):
     """Handle intent."""
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
@@ -101,7 +106,7 @@ def on_intent(intent_request, session):
 
     if session["attributes"]["current_scene"] == "begin":
         if intent_name == "OneTent":
-            session_attributes = { 
+            session_attributes = {
                 "story": "secret_story",
                 "current_scene": "01"
             }
@@ -114,25 +119,30 @@ def on_intent(intent_request, session):
             }
             speech_output = another_story.scenes["01"]["body"]
             return build_response(session_attributes, build_speechlet_response(speech_output, None, False))
-        else: 
-            speech_output = "Please say 1 or 2 to choose a story."
+        else:
+            speech_output = "Please say 1 for {} or 2 for {} to begin the adventure.".format(secret_story.title, another_story.title)
             return build_response(session["attributes"], build_speechlet_response(speech_output, None, False))                
-    
+
     story = stories[session["attributes"]["story"]]
     intent_vocab = ("YesTent", "NoTent", "UpTent", "DownTent",
                     "NorthTent", "SouthTent", "EastTent", "WestTent",
-                    "LeftTent", "RightTent", "ForwardTent", "BackTent")
-    
+                    "LeftTent", "RightTent", "ForwardTent", "BackTent",
+                    "WhatTent")
+
     if intent_name in intent_vocab:
         if intent_name in story.scenes[current]["choices"]:
             return handle_choice(story, current, intent_name, session)
-        else: 
-            speech_output = "Sorry. You can\'t do that right now."
+        else:
+            if intent_name == "WhatTent":
+                speech_output = "Sorry. I didn\'t understand that. Repeating prompt. " + story.scenes[current]["body"]
+            else:
+                speech_output = "Sorry. You can\'t do that right now. Repeating prompt. " + story.scenes[current]["body"]        
             return build_response(session["attributes"], build_speechlet_response(speech_output, None, False))
 
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
-    else: 
+        return session_end_request()
+
+    else:
         raise ValueError("Invalid intent")
 
 
@@ -143,8 +153,8 @@ def start_game():
     }
     speech_output = "Welcome to Alexa Adventures. " \
                     "Which adventure would you like to " \
-                    "play? Select 1 or 2."
-    reprompt_text = "Say 1 or 2 to begin."
+                    "play? For {} say 1. For {} say 2.".format(secret_story.title, another_story.title)
+    reprompt_text = "Please say 1 for {} or 2 for {} to begin the adventure.".format(secret_story.title, another_story.title)
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         speech_output, reprompt_text, should_end_session))
@@ -152,10 +162,15 @@ def start_game():
 
 def handle_choice(story, current, intent, session):
     """Update the story scene when a valid intent is given."""
-    next_scene = story.scenes[current]["choices"][intent]
-    
+    next_scene = story.scenes[current]["choices"][intent][0]
+    alt_text = story.scenes[current]["choices"][intent][1]
+
     if story.scenes[next_scene]["end_scene"]:
-        speech_output = story.scenes[next_scene]["body"]
+        if alt_text:
+            speech_output = story.scenes[next_scene][alt_text] + story.scenes[next_scene]["body"] + " To start a new story, say 1 or 2."
+        else:
+            speech_output = story.scenes[next_scene]["body"] + " To start a new story, say 1 or 2."
+
         session_attributes = {
             "current_scene": "begin",
         }
@@ -164,7 +179,10 @@ def handle_choice(story, current, intent, session):
             "current_scene": next_scene,
             "story": session["attributes"]["story"]
         }
-        speech_output = story.scenes[next_scene]["body"]
+        if alt_text:
+            speech_output = story.scenes[next_scene][alt_text] + story.scenes[next_scene]["body"]
+        else:
+            speech_output = story.scenes[next_scene]["body"]
     return build_response(session_attributes, build_speechlet_response(speech_output, None, False))
 
 
@@ -191,7 +209,7 @@ def build_speechlet_response(output, reprompt_text, should_end_session):
         "shouldEndSession": should_end_session
     }
 
-    
+
 def build_response(session_attributes, speechlet_response):
     """Put together attributes to be returned as a response."""
     return {
@@ -199,4 +217,3 @@ def build_response(session_attributes, speechlet_response):
         "sessionAttributes": session_attributes,
         "response": speechlet_response
     }
-    
