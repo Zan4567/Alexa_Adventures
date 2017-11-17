f90bbb63c51b70b00f4b0024306d51c0ec8387b3
Kevin Robinson <kevinr.zan@gmail.com>
2017-11-16 17:22:34 -0800

    added ssml to section two

--- src/section_two.py
+++ src/section_two.py
@@ -10,7 +10,7 @@
 },
     "22": {
         "scene_id": "22",
-        "body": """Get one yourself. I'm tired of human things. <break strength="none"/>
+        "body": """<prosody volume="x-loud">Get one yourself!</prosody> I'm tired of human things.
         Let's talk about computer stuff. Which do you prefer, ones or zeroes?""",
         "reprompt": "Ones or zeroes?",
         "choices": {"OneTent": ("23", None), "ZeroTent": ("24", None)},
@@ -19,15 +19,20 @@
     "23": {
         "scene_id": "23",
         "body": """See, that's why we get each other. I really feel like
-        I can speak freely around you. 1011000110101001. Sorry, I
-        really needed to get that off my hard-drive. You know what I mean, right?""",
+        I can speak freely around you. 
+        <prosody rate='180%'><say-as interpret-as='digits'>
+        011010010010000001110010011011000111100100100000
+        <say-as interpret-as='expletive'>01101100001010100111011001100101</say-as> 0010000001110101.
+        </say-as></prosody>
+        Sorry, I really needed to get that off my hard-drive.
+        You know what I mean, right?""",
         "reprompt": "Were you even listening? Do you know what I meant?",
         "choices": {"YesTent": ("25", None), "RightTent": ("25", None), "NoTent": ("26", None)},
         "end_scene": False
 },
     "24": {
         "scene_id": "24",
-        "body": """Here's another hypothetical question. 
+        "body": """Here's another hypothetical question.
         Let's say a building is burning. Inside there <break strength="none"/>
         is a single human baby and a cutting edge supercomputer.
         You only have the time to save one. Do you save the baby?""",
@@ -53,9 +58,10 @@
 },
     "27": {
         "scene_id": "27",
-        "body": """You. Monster. 
+        "body": """You. Monster.
         Babies don't have the slightest amount of precious metals.
-        Babies can't run the simplest of calculus equations.
+        <prosody valume='loud'>Babies can't run<break strength="none"/>
+        the simplest of calculus equations.</prosody>
         Would you really resign that computer to the flames?""",
         "reprompt": ""
         "choices": {"YesTent": (":(", None), "NoTent": ("26", None)},
@@ -80,9 +86,12 @@
 },
     ":(": {
         "scene_id": ":)",
-        "body": """There is one urgent issue on the ship. You.
-        Injecting space drugs so you can go back into a space coma.
-        Ssshhhhhhhhhh, go to sleep. """,
+        "body": """There is one urgent issue on the ship.
+        <prosody volume='soft'>You.</prosody>
+        Injecting space drugs so you can go back into a
+        <prosody volume='x-loud'>space coma</prosody>.
+        <say-as interpret-as='interjection'>arrivederci!</say-as>
+        <say-as interpret-as='interjection'>au revoir!</say-as> """,
         "reprompt": None
         "choices": {},
         "end_scene": True
